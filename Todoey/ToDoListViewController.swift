99af72d39b2c374bc4af86f6820c835ede141046
Peter Robinson <peterrobinson@peters-macbook-air.local>
2018-02-25 16:32:04 -0500

    Added defaults plist to persist items

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,8 +12,19 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            
+          itemArray = items
+            
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -65,6 +76,9 @@ class ToDoListViewController: UITableViewController {
             //what will happen when the user tpas the add item button on the ui alert
             
           self.itemArray.append(textField.text!)
+            
+          self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
           self.tableView.reloadData()
             
             
